56bc3c5d0cd4c8c2833c714477bc99b57847772d
saksham Upadhyay <saksham.upadhyay@tyutee.com>
2021-02-03 19:23:52 +0530

    Add project files.

---
+++ Abstrct/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Abstrct
+{
+
+    public abstract class printing{
+
+        public printing()
+        {
+            Console.WriteLine("This is constructor of abstract class");
+        }
+        public abstract void a_method();
+        public void normalmethod()
+        {
+            Console.WriteLine("This is a normal method of abstract class");
+        }
+
+        }
+
+    
+public class SubClass : printing
+{
+    public override void a_method()
+    {
+            Console.WriteLine("This is abstract method");
+    }
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SubClass s = new SubClass();
+            s.normalmethod();
+            s.a_method();
+        }
+    }
+}
